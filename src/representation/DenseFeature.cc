307d02225672fcf8b5c7917aa9c26da1114e363b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-16 14:51:02 +0800

    Delete src directory

--- src/representation/DenseFeature.cc
+++
@@ -1,4 +0,0 @@
-// Copyright (C) Rishabh Iyer, John T. Halloran, and Kai Wei
-// Licensed under the Open Software License version 3.0
-// See COPYING or http://opensource.org/licenses/OSL-3.0
-#include "DenseFeature.h"
--- src/utils/jensenUtils.h
+++
@@ -1,6 +0,0 @@
-// Copyright (C) Rishabh Iyer, John T. Halloran, and Kai Wei
-// Licensed under the Open Software License version 3.0
-// See COPYING or http://opensource.org/licenses/OSL-3.0
-#include "arguments.h"
-#include "error.h"
-#include "utils.h"
